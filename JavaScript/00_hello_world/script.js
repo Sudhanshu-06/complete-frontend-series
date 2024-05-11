4dddcc6aa86df2618e996a2c51340a32506fa594
Sudhanshu-06 <sudhanshu767778@gmail.com>
2024-05-11 23:26:40 +0530

    push

--- JavaScript/00_hello_world/script.js
+++ JavaScript/00_hello_world/script.js
@@ -1,3 +1,19 @@
-console.log("Hello World");
-console.log(7+8);
-console.log(5+6-4*5);
+// console.log("Hello World");
+// console.log(7+8);
+// console.log(5+6-4*5);
+// --Data types
+//Number
+let abc=7;
+console.log(typeof abc)
+// String
+let abc1="Sudhanshu"
+console.log(typeof abc1);
+// boolean
+let abc2=true;
+console.log(abc2);
+
+let abc3;
+console.log(typeof abc3);
+
+let abc4=null;
+console.log(typeof abc4);
