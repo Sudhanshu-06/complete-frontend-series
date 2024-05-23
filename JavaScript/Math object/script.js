cd1b2dde9905fcea3c080d8f5a2c58f977516664
Sudhanshu-06 <sudhanshu767778@gmail.com>
2024-05-23 17:42:30 +0530

    push

---
+++ JavaScript/Math object/script.js
@@ -0,0 +1,17 @@
+console.log(Math.sqrt(26));
+console.log(Math.pow(2,6));
+console.log(Math.PI);
+console.log(Math);
+console.log(Math.E);
+console.log(Math.floor(26.5450));
+console.log(Math.sqrt(26));
+console.log(Math.ceil(2.60));
+console.log(Math.ceil(-2.07));
+console.log(Math.round(2.65));
+console.log(Math.round(2.49));
+console.log(Math.random());
+
+const width=+prompt("please enter the rectangle width")
+const height=+prompt("please enter the rectangle height")
+console.log(width*height);
+document.write(width*height);
