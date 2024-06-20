ab5cbc479866064d805026c80a3532da463231fc
Sudhanshu-06 <sudhanshu767778@gmail.com>
2024-06-20 15:36:08 +0530

    done

---
+++ JavaScript/Object/script.js
@@ -0,0 +1,36 @@
+// const myName='Anurag'
+
+// const username1=''
+// const username2=''
+
+// const user1 = {
+//     firstName:'Akash',
+//     lastName:'Kumar',
+//     age:24,
+//     education:'Mca',
+//     city:'Noida'
+// }
+const user = {
+    firstName:'Adarsh',
+    lastName:'Kumar',
+    age:24,
+    education:'Mca',
+    address:{
+        pincode:'843329',
+        city:'Noida',
+        state:'bihar',
+        moreDetails:{
+            population:98767538567,
+            area:'787  sq km'
+        },
+    },
+    age:15,
+    isGraduate:false,
+}
+
+// console.log(user2);
+// console.log(user2["lastName"]);
+
+// user2.state='bihar'
+// user2["is-student"]=true
+// console.log(user2);
