9ac9e7df2b1fd03fb96fc74272cdb2c7bd9f0f4f
Sudhanshu-06 <sudhanshu767778@gmail.com>
2024-09-16 21:08:13 +0530

    done

---
+++ JavaScript/01_variable/script.js
@@ -0,0 +1,15 @@
+// let user="Sudhanshu"
+// let age=25
+// let isHappy=true
+// let userIntro='Hi I am ' +user;
+
+
+// const hourInDay=24;
+
+var firstName="Akash";
+let lastName="Singh";
+let age=24;
+const yearOfBirth=1999;
+
+let userIntro='Hi, my name is ' + firstName + ' ' + lastName
+console.log(userIntro);
--- JavaScript/Dom Manipulation/script.js
+++ JavaScript/Dom Manipulation/script.js
@@ -1,5 +1,58 @@
 function sayHi(){
     console.log("hi");
 }
+// document show the elemnet in html format
+// console.dir()- shows that in object format
 
 const allImages= document.getElementsByTagName('img') 
+
+// document.childeren[0].childeren[1]
+// document.body
+// document.body.children
+// document.body.children[0]
+// document.body.children[0].innerHTML='Sudhanhsu'
+// document.body.children[0].innerHTML='<i>Singh</i>'
+// document.body.children[0].innerText='Singh'
+// document.body.children[4].src=img source
+
+
+// const cssImg=document.getElementsByClassName('css-image')
+// const cssImg= document.getElementById('abcd')
+
+// let cssImg = document.querySelector('.abcd')
+// let cssImg = document.querySelector('#abcd')
+
+// let cssImg = document.querySelectorAll('#abcd')
+// let cssImg = document.querySelectorAll('.abcd')
+
+
+// document.querySelector('h1').style.color='yellow';
+// document.querySelector('h1').style.backgroundColor='aqua';
+
+
+const allLinks=document.querySelectorAll('a')
+// allLinks[0].style.color='green';
+// allLinks[1].style.color='skyblue';
+// allLinks[2].style.color='blue';
+// allLinks[3].style.color='red';
+
+// for(let i=0; i<allLinks.length; i++){
+//     allLinks[i].style.color='green';
+// }
+
+for(let link of allLinks){
+//     link.style.color='green';
+//     link.style.textDecoration='none';
+//     link.style.fontWeight=700;
+//     link.style.fontFamily='sans-serif'
+
+    link.style.cssText=`color:red;
+    font-size:18px;
+    font-family:cursive;
+    font-weight:600;
+    text-decoration:none;`
+}
+// link.classList.add('green-link')
+// link.classList.remove('green-link')
+// link.classList.toggle('green-link')
+
